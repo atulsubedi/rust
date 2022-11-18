4dc298c376e79a7c07fdf010277cad16b5cad32f
atulya <atul.work15@gmail.com>
2022-11-18 16:15:03 +0545

    main.rs file modified

--- main.rs
+++ main.rs
@@ -1,11 +1,14 @@
-// made clippy more sensitive to be sure the code is awesome
-#![warn(clippy::all, clippy::pedantic)]
+// using use keyword so that we don't have to type out the full version every time while taking
+// input from the terminal
+use std::io::stdin;
 fn main(){
-    let my_list = ["one","two","three","four"];
-    for _num in &my_list {
-        println!("{ }",_num);
-    }
-    for i in 0..4 {
-        println!("{ }",i);
-    } 
+   println!("Hello, what's your name?");
+   // mut lets you to change the value of variable 
+   let mut your_name = String::new();
+    // stdin() returns an object granting access to the Standard input
+   stdin() 
+       // read_line() is a method it receives input from keyboard
+       .read_line(&mut your_name)
+       .expect("Failed to read line");
+   println!("Hello, { }",your_name)
 }
