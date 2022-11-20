f4b894ce14a9bcb8663b07128c4c5f5d644928fa
atulya <atul.work15@gmail.com>
2022-11-20 22:26:50 +0545

    in the list debugged

--- main.rs
+++ main.rs
@@ -6,19 +6,20 @@ use std::io::stdin;
          .read_line(&mut your_name)
          .expect("Failed to read line");
      your_name
-         .to_lowercase()
          .trim()
+         .to_lowercase()
+
  }
 fn main(){
     println!("Hello, what's your name?");
     let name = what_is_your_name();
     println!("{}",name);
 
-    let visitor_list : [&str;2] = ["atul","hl"];
+    let visitor_list = ["atul","hl"];
 
     // rust for loop operates on ranges rather than a set of numbers
      let mut allow_them_in = false;
-    for i in visitor_list.len(){
+    for visitor in &visitor_list {
         if visitor == &name{
             allow_them_in = true;
         }
