2bbea0be82956cd6d29b12c5f0c3226e08799300
atulya <atul.work15@gmail.com>
2022-11-20 21:48:26 +0545

    created new list

--- main.rs
+++ main.rs
@@ -1,24 +1,12 @@
 use std::io::stdin;
 // -> String denotes that it returns a string
-fn what_is_your_name() -> String{
-    let mut your_name = String::new();
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failed to read line");
-    your_name
-        .trim()
-        .to_lowercase()
-} 
-
+ 
 fn main(){
-    println!("Hello, what's your name?");
-    // calling the function what is your name to store the result in name
-    let name = what_is_your_name();
-    println!("{:?}", name);
-        
- if name  == "atul" || name == "hl"{
-    println!("Welcome");
-} else {
-    println!("Sorry, you are not on the list")
+    let visitor_list : [&str;3] = ["atul","atulhl","hl"];
+    // rust for loop operates on ranges rather than a set of numbers
+     
+
 }
-}
+
+
+
